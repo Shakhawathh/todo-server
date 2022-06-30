9a9f13515516cf2865d20e0fb46b90b97b0b0f5d
Shakhawathh <shakhawathh55@gmail.com>
2022-07-01 01:26:40 +0600

    herko

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ async function run() {
 
         app.delete('/service/:id', async(req, res) =>{
             const id =req.params.id
-            const query = {_id:ObjectID(id)}
+            const query = {_id:ObjectId(id)}
             const result= await servicesCollection.deleteOne(query)
             res.send(result)
-        })
+        });
         
     }
     finally {
