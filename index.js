3394bd7046c05fe4a987708a6bf643295eb58e2a
Shakhawathh <shakhawathh55@gmail.com>
2022-07-01 00:47:21 +0600

    server adde

--- index.js
+++ index.js
@@ -12,10 +12,50 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2yyl.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const servicesCollection = client.db('todo-list').collection('services')
+
+        
+
+        app.post('/service', async (req, res) => {
+            const service = req.body
+            const result = servicesCollection.insertOne(service )
+            res.send(result)
+        })
+
+        app.get('/service', async(req, res) =>{
+            const query = {}
+            const cursor = servicesCollection.find(query)
+            const services = await cursor. toArray()
+            res.send(services)
+        })
+
+        app.delete('/service/:id', async(req, res) =>{
+            const id =req.params.id
+            const query = {_id:ObjectID(id)}
+            const result= await servicesCollection.deleteOne(query)
+            res.send(result)
+        })
+        
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello todo!')
+    res.send('Hello todo!')
 })
 
 app.listen(port, () => {
-  console.log(`todo listening on port ${port}`)
+    console.log(`todo listening on port ${port}`)
 })
